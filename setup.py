bfa558fbea4663a45d28fa467bd79c577e32f92d
Eric Lee <eleenest@google.com>
2018-05-20 20:55:17 -0700

    Create setup.py file, test uploading, etc.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='elee_testCodingQuestions_01',
-    version='0.0.1',
-    packages=['venv.lib.python2.7.distutils'],
+    name='elee_testCodingQuestions',
+    version='0.0.4',
+    packages=['repeating_numbers'],
     url='https://github.com/el-abcd/test01',
     license='MIT',
     author='el-abcd',
     author_email='e_l2.fake@hotmail.com',
-    description='Simple dummy upload.  Try things out.  '
+    description='Simple dummy upload.  Try things out.  ',
+    long_description=open('README.md').read(),
 )
